31de2060f5db8cccc99f02465d34ba39b59c9114
Leesamgsoo <sslee3680035@gmail.com>
2024-10-28 12:45:33 +0900

    use rs data

--- src/main/java/com/restapi/example/article/controller/ApiV1ArticleController.java
+++ src/main/java/com/restapi/example/article/controller/ApiV1ArticleController.java
@@ -3,6 +3,9 @@ package com.restapi.example.article.controller;
 import com.restapi.example.article.dto.ArticleDTO;
 import com.restapi.example.article.entity.Article;
 import com.restapi.example.article.service.ArticleService;
+import com.restapi.example.global.RsData.RsData;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,39 +19,58 @@ public class ApiV1ArticleController {
     private final ArticleService articleService;
     private List<ArticleDTO> articleDTOList;
 
-    @GetMapping("")
-    public List<ArticleDTO> list() {
-        articleDTOList = new ArrayList<>();
-        articleDTOList.add(new ArticleDTO(new Article("제목1", "내용1")));
-        articleDTOList.add(new ArticleDTO(new Article("제목2", "내용2")));
-        articleDTOList.add(new ArticleDTO(new Article("제목3", "내용3")));
+    @Getter
+    @AllArgsConstructor
+    public static class ArticlesResponse {
+        private final List<ArticleDTO> articleList;
+    }
 
-        return articleDTOList;
+    @Getter
+    @AllArgsConstructor
+    public static class ArticleResponse {
+        private final ArticleDTO article;
+    }
+
+    @GetMapping("")
+    public RsData<ArticlesResponse> list() {
+        List<ArticleDTO> articleList = new ArrayList<>();
+
+        Article article1 = new Article("제목1", "내용1");
+        articleList.add(new ArticleDTO(article1));
+
+        Article article2 = new Article("제목2", "내용2");
+        articleList.add(new ArticleDTO(article2));
+
+        Article article3 = new Article("제목3", "내용3");
+        articleList.add(new ArticleDTO(article3));
+
+        return RsData.of("200", "게시글 다건 조회 성공", new ArticlesResponse(articleList));
     }
 
     @GetMapping("/{id}")
-    public ArticleDTO getArticle(@PathVariable("id") Long id) {
-        return articleDTOList.get(Math.toIntExact(id));
+    public RsData<ArticleResponse> getArticle(@PathVariable("id") Long id) {
+        return RsData.of("200", "게시글 단건 조회 성공",
+                new ArticleResponse(new ArticleDTO(new Article("제목1", "내용1"))));
     }
 
     @PostMapping("")
-    public ArticleDTO create(@RequestParam("subject") String subject,
-                         @RequestParam("content") String content) {
-        return new ArticleDTO(new Article(subject, content));
+    public String create(@RequestParam("subject") String subject, @RequestParam("content") String content) {
+        System.out.println(subject);
+        System.out.println(content);
+        return "등록완료";
     }
 
     @PatchMapping("/{id}")
-    public String modify(@RequestParam("subject") String subject,
-                         @RequestParam("content") String content,
-                         @PathVariable("id") Long id) {
+    public String modify(@PathVariable("id") Long id, @RequestParam("subject") String subject, @RequestParam("content") String content) {
+        System.out.println(id);
         System.out.println(subject);
         System.out.println(content);
-        return id + "번 수정성공";
+        return "수정완료";
     }
 
     @DeleteMapping("/{id}")
     public String delete(@PathVariable("id") Long id) {
-        System.out.println(id + "삭제 됨");
-        return id + "번 삭제성공";
+        System.out.println(id);
+        return "삭제완료";
     }
 }
